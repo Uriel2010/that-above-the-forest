b2b0f2152329e3f35386a26257b83d9be97bf954
delfinagamenara <delfigamenara@gmail.com>
2026-08-11 16:37:23 -0300

    funciona el fps

--- Assets/Scripts/FPS.cs
+++ Assets/Scripts/FPS.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FPS : MonoBehaviour
 {
-    CharacterController characterController
+    CharacterController characterController;
 
     public float walkspeed = 6.0f;
     public float runSpeed = 10.0f;
@@ -28,17 +28,17 @@ public class FPS : MonoBehaviour
     void Update () 
     {
     
-    h_mouse = mouseHorizontal * InputGetAxis("Mouse X");
-    v_mouse += mouseVertical * InputGetAxis("Mouse Y");
+    h_mouse = mouseHorizontal * Input.GetAxis("Mouse X");
+    v_mouse += mouseVertical * Input.GetAxis("Mouse Y");
    
-    v_mouse = Math.Clamp(v_mouse, minRotation, maxRotation);
+    v_mouse = Mathf.Clamp(v_mouse, minRotation, maxRotation);
     cam.transform.localEulerAngles = new Vector3(-v_mouse, 0, 0);
 
     if (characterController.isGrounded) 
         {
             move = new Vector3 (Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         if(Input.GetKey(KeyCode.LeftShift))
-            move = transform.TransformDirection(move) * runSpeedpeed;
+            move = transform.TransformDirection(move) * runSpeed;
         else
             move = transform.TransformDirection(move) * walkspeed;
         
