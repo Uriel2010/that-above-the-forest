37f03c44ab772c253c78dff28c63818d7815c634
Uriel2010 <50322307@est.ort.edu.ar>
2026-08-26 14:04:29 -0300

    a

--- Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
+++ Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -1,6 +1,8 @@
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using TMP_Text;
+using TMPro;
 
 #pragma warning disable 618
 namespace UnityStandardAssets.Utility
@@ -9,7 +11,7 @@ namespace UnityStandardAssets.Utility
     {
         // An incredibly simple menu which, when given references
         // to gameobjects in the scene
-        public Text camSwitchButton;
+        public TMPro.TMP_Text camSwitchButton;
         public GameObject[] objects;
 
 
