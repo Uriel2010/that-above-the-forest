88fe6fc99fbe3b5c07c7d1d5c9687b8c1614da62
Uriel2010 <50322307@est.ort.edu.ar>
2026-08-31 09:51:32 -0300

    arreglando cosas

--- Assets/Scripts/LanternController.cs
+++ Assets/Scripts/LanternController.cs
@@ -1,34 +1,23 @@
 using UnityEngine;
-using TMPro;
 
 public class LanternController : MonoBehaviour
 {
     [SerializeField] private Light lantern;
 
-    [SerializeField] private float intensidadAlta = 20f;
+    [SerializeField] private float intensidadAlta = 15f;
     [SerializeField] private float intensidadBaja = 3f;
     [SerializeField] private float rangoAlto = 20f;
     [SerializeField] private float rangoBajo = 10f;
     [SerializeField] private float anguloAlto = 100f;
     [SerializeField] private float anguloBajo = 90f;
 
-    [SerializeField] private float tiempoMaximo = 100f;
-
-    // Texto del contador
-    [SerializeField] private TMP_Text textoContador;
-
-    private float tiempoRestante;
-    private bool modoAlto = false;
+    private bool modoAlto = true;
 
     void Start()
     {
-        tiempoRestante = tiempoMaximo;
-
         lantern.intensity = intensidadBaja;
         lantern.range = rangoBajo;
         lantern.spotAngle = anguloBajo;
-
-        ActualizarContador();
     }
 
     void Update()
@@ -36,53 +25,24 @@ public class LanternController : MonoBehaviour
         if (lantern == null)
             return;
 
-        // Click derecho
         if (Input.GetMouseButtonDown(1))
         {
-            // Pasar a intensidad alta
-            if (!modoAlto && tiempoRestante > 0)
-            {
-                modoAlto = true;
+            modoAlto = !modoAlto;
 
+            if (modoAlto)
+            {
                 lantern.intensity = intensidadAlta;
                 lantern.range = rangoAlto;
                 lantern.spotAngle = anguloAlto;
             }
-            // Pasar a intensidad baja
-            else if (modoAlto)
+            else
             {
-                modoAlto = false;
-
-                lantern.intensity = intensidadBaja;
-                lantern.range = rangoBajo;
-                lantern.spotAngle = anguloBajo;
-            }
-        }
-
-        // Consumir tiempo mientras está en intensidad alta
-        if (modoAlto)
-        {
-            tiempoRestante -= Time.deltaTime;
-
-            if (tiempoRestante <= 0)
-            {
-                tiempoRestante = 0;
-                modoAlto = false;
-
                 lantern.intensity = intensidadBaja;
                 lantern.range = rangoBajo;
                 lantern.spotAngle = anguloBajo;
             }
 
-            ActualizarContador();
-        }
-    }
-
-    void ActualizarContador()
-    {
-        if (textoContador != null)
-        {
-            textoContador.text = Mathf.CeilToInt(tiempoRestante).ToString();
+            Debug.Log("Intensidad de linterna: " + lantern.intensity);
         }
     }
 }
