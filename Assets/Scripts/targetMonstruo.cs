15dff7cce0aebc9313183e36bcaf2e0b867b02a7
delfinagamenara <delfigamenara@gmail.com>
2026-09-16 08:06:15 -0300

    hola

--- Assets/Scripts/targetMonstruo.cs
+++ Assets/Scripts/targetMonstruo.cs
@@ -2,25 +2,15 @@ using UnityEngine;
 
 public class targetMonstruo : MonoBehaviour
 {
-    [Header("Referencias")]
-    public Transform playerPosition;
-    public Transform monstruo;   
-
-    private bool congelado = false;
+   public Transform playerPosition; 
 
     void Start()
     {
-        transform.position = playerPosition.position;
+        
     }
 
     void Update()
     {
-        monstruo = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        transform.position = congelado ? monstruo.position : playerPosition.position;
-    }
-
-    public void SetCongelado(bool valor)
-    {
-        congelado = valor;
+        transform.position = playerPosition.position;
     }
 }
